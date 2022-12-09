e93fab4d4a307e926b30edf65385708e51c16d8b
Kalim <80146968+Kalim22@users.noreply.github.com>
2022-12-09 18:37:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+// Scrolling navbar
+
+// let header = document.querySelector("header");
+// window.addEventListener("scroll", () => {
+//   if (window.pageYOffset > 10) {
+//     header.style.position = "fixed";
+//     header.style.top = 0;
+//     header.style.background = "white";
+//   } else {
+//     header.style.position = "relative";
+//   }
+// });
+
+// let heroSectionLoader = document.querySelector(".hero-section-loader");
+// window.addEventListener("load", () => {
+//   heroSectionLoader.style.transform = "translateX(-100%)";
+// });
