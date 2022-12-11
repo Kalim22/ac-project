a40be4d53c3e0d08a309a1cb529bd35aa9a2559f
kalim22 <kalimali2295@gmail.com>
2022-12-11 11:50:48 +0530

    index page updated

--- index.js
+++ index.js
@@ -15,3 +15,26 @@
 // window.addEventListener("load", () => {
 //   heroSectionLoader.style.transform = "translateX(-100%)";
 // });
+
+// scrolling services cards
+
+let servicesCards1 = document.querySelector(".card1");
+let servicesCards2 = document.querySelector(".card2");
+let servicesCards3 = document.querySelector(".card3");
+let servicesCards4 = document.querySelector(".card4");
+
+let heroSectionContent = document.querySelector(".hero-section-content");
+
+window.addEventListener("scroll", () => {
+  let scroll = window.pageYOffset;
+  let scrollVal = scroll * 0.5;
+  let scrollOpacityVal = scroll * 0.0021;
+
+  servicesCards1.style.left = `${scrollVal * 0.001}%`;
+  servicesCards2.style.left = `${scrollVal * 0.039}%`;
+  servicesCards3.style.left = `${scrollVal * 0.076}%`;
+  servicesCards4.style.left = `${scrollVal * 0.112}%`;
+
+  heroSectionContent.style.opacity = `${1 - scrollOpacityVal}`;
+  heroSectionContent.style.transform = `translateY(${scrollVal * 0.3}%)`;
+});
